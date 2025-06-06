20a12b0cc8f2a4ce29b55c2f5576964a2a79215b
kaleab Tariku <Kaleab-Tariku@users.noreply.github.com>
2025-06-06 19:52:00 +0300

    first commit

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+i = input("What's the weather like today? (sunny/rainy/cold): ")
+if i =="sunny":
+    print("Wear a t-shirt and sunglasses.")
+elif i =="rainy":
+    print("Don't forget your umbrella and a raincoat.")   
+elif i =="cold":
+    print("Make sure to wear a warm coat and a scarf.")    
+else:
+    print("Sorry, I don't have recommendations for this weather.")    
